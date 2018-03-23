projectacrn/acrn-kernel

drm/i915/icl: Icelake 11.5 only has global MOCS registers
Until Icelake, each engine had its own set of 64 MOCS registers. In
order to simplify, Icelake 11.5 moves to only 64 Global MOCS registers,
which are no longer part of the engine context. Since these registers
are now global, they also only need to be initialized once.

These new global MOCS registers are located in the same offset of the
render MOCS register from previous platforms.

v2: Advertise global mocs registers as a platform feature flag;
initialize global mocs registers outside the per-engine init  (Daniele).

v3: Move IS_ICELAKE() condition in get_mocs_settings to a preceding
gen11.0 patch (tomaszli)

HSD: 399647
Cc: Oscar Mateo Lozano <oscar.mateo@intel.com>
Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
Cc: Tomasz Lis <tomasz.lis@intel.com>
Signed-off-by: Michel Thierry <michel.thierry@intel.com>
Reviewed-by: Oscar Mateo Lozano <oscar.mateo@intel.com>

drivers/gpu/drm/i915/intel_mocs.c
@@ -241,6 +241,10 @@ int intel_mocs_init_engine(struct intel_engine_cs *engine)
 	struct drm_i915_mocs_table table;
 	unsigned int index;
 
+	/* Platforms with global MOCS do not need per-engine initialization. */
+	if (HAS_GLOBAL_MOCS_REGISTERS(dev_priv))
+		return 0;
+
 	if (!get_mocs_settings(dev_priv, &table))
 		return 0;
 
@@ -261,6 +265,47 @@ int intel_mocs_init_engine(struct intel_engine_cs *engine)
 	 */
 	for (; index < GEN9_NUM_MOCS_ENTRIES; index++)
 		I915_WRITE(mocs_register(engine->id, index),
+			   table.table[0].control_value);
+
+	return 0;
+}
+
+/**
+ * intel_mocs_init_global() - program the global mocs registers
+ * @dev_priv:      i915 device private.
+ *
+ * This function initializes the MOCS global registers.
+ * Must be used only in platforms with has_global_mocs.
+ *
+ * Return: 0 on success, otherwise the error status.
+ */
+int intel_mocs_init_global(struct drm_i915_private *dev_priv)
+{
+	struct drm_i915_mocs_table table;
+	unsigned int index;
+
+	GEM_BUG_ON(!HAS_GLOBAL_MOCS_REGISTERS(dev_priv));
+
+	if (!get_mocs_settings(dev_priv, &table))
+		return 0;
+
+	if (WARN_ON(table.size > GEN9_NUM_MOCS_ENTRIES))
+		return -ENODEV;
+
+	for (index = 0; index < table.size; index++)
+		I915_WRITE(GEN11_5_GLOBAL_MOCS(index),
+			   table.table[index].control_value);
+
+	/*
+	 * Ok, now set the unused entries to uncached. These entries
+	 * are officially undefined and no contract for the contents
+	 * and settings is given for these entries.
+	 *
+	 * Entry 0 in the table is uncached - so we are just writing
+	 * that value to all the used entries.
+	 */
+	for (; index < GEN9_NUM_MOCS_ENTRIES; index++)
+		I915_WRITE(GEN11_5_GLOBAL_MOCS(index),
 			   table.table[0].control_value);
 
 	return 0;
@@ -438,6 +483,9 @@ int intel_rcs_context_init_mocs(struct drm_i915_gem_request *req)
 {
 	struct drm_i915_mocs_table t;
 	int ret;
+
+	if (HAS_GLOBAL_MOCS_REGISTERS(req->i915))
+		return 0;
 
 	if (get_mocs_settings(req->i915, &t)) {
 		/* Program the RCS control registers */

keyonjie/linux

drivers/gpu/drm/i915/intel_mocs.c
@@ -241,6 +241,10 @@ int intel_mocs_init_engine(struct intel_engine_cs *engine)
 	struct drm_i915_mocs_table table;
 	unsigned int index;
 
+	/* Platforms with global MOCS do not need per-engine initialization. */
+	if (HAS_GLOBAL_MOCS_REGISTERS(dev_priv))
+		return 0;
+
 	if (!get_mocs_settings(dev_priv, &table))
 		return 0;
 
@@ -261,6 +265,47 @@ int intel_mocs_init_engine(struct intel_engine_cs *engine)
 	 */
 	for (; index < GEN9_NUM_MOCS_ENTRIES; index++)
 		I915_WRITE(mocs_register(engine->id, index),
+			   table.table[0].control_value);
+
+	return 0;
+}
+
+/**
+ * intel_mocs_init_global() - program the global mocs registers
+ * @dev_priv:      i915 device private.
+ *
+ * This function initializes the MOCS global registers.
+ * Must be used only in platforms with has_global_mocs.
+ *
+ * Return: 0 on success, otherwise the error status.
+ */
+int intel_mocs_init_global(struct drm_i915_private *dev_priv)
+{
+	struct drm_i915_mocs_table table;
+	unsigned int index;
+
+	GEM_BUG_ON(!HAS_GLOBAL_MOCS_REGISTERS(dev_priv));
+
+	if (!get_mocs_settings(dev_priv, &table))
+		return 0;
+
+	if (WARN_ON(table.size > GEN9_NUM_MOCS_ENTRIES))
+		return -ENODEV;
+
+	for (index = 0; index < table.size; index++)
+		I915_WRITE(GEN11_5_GLOBAL_MOCS(index),
+			   table.table[index].control_value);
+
+	/*
+	 * Ok, now set the unused entries to uncached. These entries
+	 * are officially undefined and no contract for the contents
+	 * and settings is given for these entries.
+	 *
+	 * Entry 0 in the table is uncached - so we are just writing
+	 * that value to all the used entries.
+	 */
+	for (; index < GEN9_NUM_MOCS_ENTRIES; index++)
+		I915_WRITE(GEN11_5_GLOBAL_MOCS(index),
 			   table.table[0].control_value);
 
 	return 0;
@@ -438,6 +483,9 @@ int intel_rcs_context_init_mocs(struct i915_request *rq)
 {
 	struct drm_i915_mocs_table t;
 	int ret;
+
+	if (HAS_GLOBAL_MOCS_REGISTERS(rq->i915))
+		return 0;
 
 	if (get_mocs_settings(rq->i915, &t)) {
 		/* Program the RCS control registers */