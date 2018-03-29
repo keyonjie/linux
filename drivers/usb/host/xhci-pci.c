e26dda5b0939f940f9359881b565eb11df2206c4
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2018-03-29 16:04:00 -0700

    usb: xhci-pci: Add XHCI_INTEL_USB_ROLE_SW quirk for APL SOC
    In APL SOC, we have an internal MUX that's shared between host and
    device controllers. So we need a software based role switch support to
    control this internal MUX. This patch adds required quirks to enable
    this support.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -180,6 +180,11 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 		xhci->quirks |= XHCI_SSIC_PORT_UNUSED;
 		xhci->quirks |= XHCI_INTEL_USB_ROLE_SW;
 	}
+
+	if (pdev->vendor == PCI_VENDOR_ID_INTEL &&
+	     pdev->device == PCI_DEVICE_ID_INTEL_APL_XHCI)
+		xhci->quirks |= XHCI_INTEL_USB_ROLE_SW;
+
 	if (pdev->vendor == PCI_VENDOR_ID_INTEL &&
 	    (pdev->device == PCI_DEVICE_ID_INTEL_CHERRYVIEW_XHCI ||
 	     pdev->device == PCI_DEVICE_ID_INTEL_APL_XHCI ||
